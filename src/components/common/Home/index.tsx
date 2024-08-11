307ee9e369794c3a8dc05b15485bcdf102ee8b6c
pktherock <59223750+pktherock@users.noreply.github.com>
2024-08-11 23:56:32 +0530

    Add missing tailwind setup

--- src/components/common/Home/index.tsx
+++ src/components/common/Home/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import reactLogo from '@/assets/react.svg';
 import viteLogo from '/vite.svg';
-import './index.css';
 
 const Home = () => {
   const [count, setCount] = useState(0);
@@ -21,7 +20,7 @@ const Home = () => {
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="bg-red-300">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
